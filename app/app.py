f20654ff47fa0f4436bc5750382a5db5754affc8
Paul Okolie <paulokolie909@gmail.com>
2024-03-05 23:15:43 +0100

    Dockerfile working

--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,4 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
